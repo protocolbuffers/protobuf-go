0232edc1d77867cb7425954dfc6759c90e552d97
Damien Neil <dneil@google.com>
2020-02-20 10:30:38 -0800

    all: remove stray "." from license headers
    Remove a stray bit of punctuation that crept into one of the license
    headers and got copied around everywhere.
    
    Change-Id: Iebe4e882650ab6dab28f132b5e324e2ab0b99a73
    Reviewed-on: https://go-review.googlesource.com/c/protobuf/+/220339
    Reviewed-by: Joe Tsai <thebrokentoaster@gmail.com>

--- internal/encoding/messageset/messageset.go
+++ internal/encoding/messageset/messageset.go
@@ -1,5 +1,5 @@
 // Copyright 2019 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // Package messageset encodes and decodes the obsolete MessageSet wire format.
