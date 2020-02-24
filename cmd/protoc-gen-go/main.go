764a9d16a64a5b5b2cf2f267e376e4c17ffe4612
Joe Tsai <joetsai@digital-static.net>
2020-02-24 13:25:08 -0800

    cmd/protoc-gen-go: add link to devsite for protoc-gen-go
    Change-Id: I67a0897df42e177782ec76bb307ea2334f86975e
    Reviewed-on: https://go-review.googlesource.com/c/protobuf/+/220596
    Reviewed-by: Damien Neil <dneil@google.com>

--- cmd/protoc-gen-go/main.go
+++ cmd/protoc-gen-go/main.go
@@ -2,8 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// The protoc-gen-go binary is a protoc plugin to generate a Go protocol
-// buffer package.
+// The protoc-gen-go binary is a protoc plugin to generate Go code for
+// both proto2 and proto3 versions of the protocol buffer language.
+//
+// For more information about the usage of this plugin, see:
+//	https://developers.google.com/protocol-buffers/docs/reference/go-generated
 package main
 
 import (
