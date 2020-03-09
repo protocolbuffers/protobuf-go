ea6e3da5ef806be0b247b9fb3e8d0ceca6262a5c
Joe Tsai <joetsai@digital-static.net>
2020-03-09 12:07:42 -0700

    internal/testprotos/legacy: avoid direct dependency on github.com/golang/protobuf
    Change-Id: I42b17ae672a76d91ecbeeb2c12949002d7720524
    Reviewed-on: https://go-review.googlesource.com/c/protobuf/+/222677
    Reviewed-by: Damien Neil <dneil@google.com>

--- internal/testprotos/legacy/bug1052/bug1052.pb.go
+++ internal/testprotos/legacy/bug1052/bug1052.pb.go
@@ -11,8 +11,8 @@ import (
 	fmt "fmt"
 	math "math"
 
-	proto "github.com/golang/protobuf/proto"
-	descriptor "github.com/golang/protobuf/protoc-gen-go/descriptor"
+	proto "google.golang.org/protobuf/internal/protolegacy"
+	descriptor "google.golang.org/protobuf/types/descriptorpb"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
