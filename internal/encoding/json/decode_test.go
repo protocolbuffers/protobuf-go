f01a588e5810b90996452eec4a28f22a0afae023
Damien Neil <dneil@google.com>
2024-03-05 08:54:24 -0800

    encoding/protojson, internal/encoding/json: handle missing object values
    In internal/encoding/json, report an error when encountering a }
    when we are expecting an object field value. For example, the input
    `{"":}` now correctly results in an error at the closing } token.
    
    In encoding/protojson, check for an unexpected EOF token in
    skipJSONValue. This is redundant with the check in internal/encoding/json,
    but adds a bit more defense against any other similar bugs that
    might exist.
    
    Fixes CVE-2024-24786
    
    Change-Id: I03d52512acb5091c8549e31ca74541d57e56c99d
    Reviewed-on: https://go-review.googlesource.com/c/protobuf/+/569356
    TryBot-Bypass: Damien Neil <dneil@google.com>
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    Commit-Queue: Damien Neil <dneil@google.com>

--- internal/encoding/json/decode_test.go
+++ internal/encoding/json/decode_test.go
@@ -1194,6 +1194,21 @@ func TestDecoder(t *testing.T) {
 			},
 		},
 		{
+			in: `{""`,
+			want: []R{
+				{V: ObjectOpen},
+				{E: errEOF},
+			},
+		},
+		{
+			in: `{"":`,
+			want: []R{
+				{V: ObjectOpen},
+				{V: Name{""}},
+				{E: errEOF},
+			},
+		},
+		{
 			in: `{"34":"89",}`,
 			want: []R{
 				{V: ObjectOpen},
