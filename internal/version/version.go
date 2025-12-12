96a179180f0ad6bba9b1e7b6e38d0affb0168e9a
Michael Stapelberg <stapelberg@golang.org>
2025-12-12 09:38:44 +0100

    all: release v1.36.11
    Change-Id: I451fcbd61dc2be12f9df9cb8efd44d2649307b17
    Reviewed-on: https://go-review.googlesource.com/c/protobuf/+/729640
    Reviewed-by: Lasse Folger <lassefolger@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -52,8 +52,8 @@ import (
 const (
 	Major      = 1
 	Minor      = 36
-	Patch      = 10
-	PreRelease = "devel"
+	Patch      = 11
+	PreRelease = ""
 )
 
 // String formats the version string for this module in semver format.
